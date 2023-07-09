f011394394a8bea4e387401eb701a84d49cd597c
Kaleab Bayih <kaleabbyh@gmail.com>
2023-07-09 13:25:20 +0300

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -11,10 +11,10 @@ const Login = () => {
   });
 
 
-  //   const [credentials, setCredentials] = useState({
-  //   username: undefined,
-  //   password: undefined,
-  // });
+    const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
 
   const { loading, error, dispatch } = useContext(AuthContext);
 
