c0367cd043cc25540dd1bdbde2bd4a4491e0d7ba
Kaleab Bayih <kaleabbyh@gmail.com>
2023-08-09 01:28:11 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/home/Home";
+// import Hotel from "./pages/hotel/Hotel";
+// import List from "./pages/list/List";
+// import Login from "./pages/login/Login";
+
 function App() {
   return (
     <BrowserRouter>
