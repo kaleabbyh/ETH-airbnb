3745fc53871ac96d190b4a3e36288e8e607c7dab
Kaleab Bayih <kaleabbyh@gmail.com>
2023-09-19 23:42:34 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,16 +11,6 @@ import cors from "cors";
 
 
 
-// import express from "express";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import authRoute from "./routes/auth.js";
-// import usersRoute from "./routes/users.js";
-// import hotelsRoute from "./routes/hotels.js";
-// import roomsRoute from "./routes/rooms.js";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-
 const app = express();
 dotenv.config();
 
